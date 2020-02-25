ed9c824f6b9489b1b8a18fb03597512e0cf0dd92
Revati Tekale <revitekale1910@gmail.com>
2020-02-25 16:23:54 +0530

    ADDED:Employee is Present or not

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wages Computation"
 
+#variables
+isPresent=1
+
+attendance()
+{
+  if [[ $((RANDOM%2)) -eq $isPresent ]]
+  then
+       echo "Employee is present"
+  else
+       echo "Employee is not present"
+  fi
+}
+attendance
